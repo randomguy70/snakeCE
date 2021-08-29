8110263d68b267b39c1e82644ad879d1c8d229c6
randomguy70 <onlychessfreeze0515@gmail.com>
2021-08-28 21:58:38 -0500

    updated stuff

--- src/colors.h
+++ src/colors.h
@@ -1,10 +1,6 @@
 #ifndef COLORS_H
 #define COLORS_H
 
-#define BLACK        0
-#define WHITE        1
-#define TRANSPARENT  2
-#define NEON_ORANGE  3
-#define MED_GREY     4
+#define BLACK 0
 
 #endif
