8a330eb69d3e9614537accf47c96715b32a8adbe
randomguy70 <onlychessfreeze0515@gmail.com>
2021-09-05 22:08:54 -0500

    updated stuff (auto message)

--- src/save.h
+++ src/save.h
@@ -1,5 +1,23 @@
+#ifndef SAVE_H
+#define SAVE_H
+
 #include "settings.h"
 
 #define SAVE_APPVAR SNKSAVE
 
+// save settings and score
+bool saveState(struct settings* settings, uint8_t score);
 
+// load settings and score
+int loadState(struct settings *settings);
+
+// stores unencrypted score into given pointer
+int getScore(uint8_t *score);
+
+// checks the veracity of the save appvar
+int checkSaveFileAuthenticity(void);
+
+// wipes the save appvar (obv)
+void resetSaveFile(void);
+
+#endif
