5955b5aad9c70bdae275e0734b54dc4d41527656
randomguy70 <onlychessfreeze0515@gmail.com>
2021-09-09 19:55:10 -0500

    updated stuff (auto message)

--- src/main.c
+++ src/main.c
@@ -80,9 +80,13 @@ int main(void) {
 void handlePresses(struct snake *snake) {
 	if(kb_IsDown(kb_KeyUp) && snake->direction!=DIR_DOWN) {
 		snake->direction = DIR_UP;
+		if(kb_IsDown(kb_KeyRight) || kb_IsDown(kb_KeyLeft))
+			return;
 	}
 	if(kb_IsDown(kb_KeyDown) && snake->direction!=DIR_UP) {
 		snake->direction = DIR_DOWN;
+		if(kb_IsDown(kb_KeyRight) || kb_IsDown(kb_KeyLeft))
+			return;
 	}
 	if(kb_IsDown(kb_KeyRight) && snake->direction!=DIR_LEFT) {
 		snake->direction = DIR_RIGHT;
@@ -107,14 +111,14 @@ int menu(uint8_t score) {
 	gfx_SetColor(BLACK); gfx_FillRectangle_NoClip(x, y, width, height);
 	gfx_SetColor(GREY);  thickRectangle(x, y, width, height, 3);
 	
-	printColoredString("Score:", x+10, y+6, &color);
+	printColoredString("Score:", x+10, y+10, &color);
 	gfx_PrintUInt((unsigned int) score, 2);
-	printColoredString("High:", (x + width) - (30+ gfx_GetStringWidth("High:")), y+6, &color);
+	printColoredString("High:", (x + width) - (30+ gfx_GetStringWidth("High:")), y+10, &color);
 	gfx_PrintUInt((unsigned int) getHighScore(), 2);
 	
 	for(uint8_t i=0; i<numTxtStrings; i++) {
 		int strX = LCD_WIDTH/2 - gfx_GetStringWidth(txt[i])/2;
-		int strY = y + 20 + i*lineSpacing;
+		int strY = y + 30 + i*lineSpacing;
 		printColoredString(txt[i], strX, strY, &color);
 	}
 	
